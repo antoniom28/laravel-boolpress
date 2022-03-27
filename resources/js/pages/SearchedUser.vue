0abfc35e22911a8327887f63345679f288e72807
antoniom28 <antomarc@gmail.com>
2022-03-27 17:29:47 +0200

    sistemato avatar

--- resources/js/pages/SearchedUser.vue
+++ resources/js/pages/SearchedUser.vue
@@ -159,7 +159,7 @@ export default {
             transform: translate(-50% , -50%);
 
             img{
-                width: 100%;
+                height: 100%;
                 position: absolute;
                 top: 50%;
                 left: 50%;
